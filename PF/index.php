26228122c968b298fdc67cdac1be61dd0cc1cc01
ikory-leo <ikory-leo@outlook.com>
2017-06-01 15:11:31 -0500

    Add files via upload
    cuadrito

--- PF/index.php
+++ PF/index.php
@@ -10,24 +10,18 @@ and open the template in the editor.
         <title>ProyectoFinal</title>
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="estilos.css">
+        
     </head>
-    <body style="background-color:cornsilk;">
+    <body style="background-color:#3db9a9;">
          <script>
-             mensaje();
-	function mensaje()
-	{
-	       alert ("BIENVENIDO AL SEGUNDO HOGAR DE SU MASCOTA ¡¡K9 SUITE!!");    
-        }
-        function nueva(url){
-            window.location.href = url;
-        }
+
 </script>
 <form onsubmit="return mensaje()" action="signIn.php" >
     <div class='miBoton'>
-<input type="submit" value="SIGN IN" class='btn btn-info'/>  
+<input type="submit" value="SIGN IN" class='btn btn-info'/>
     </div>
     <div>
-<label id="titulo">HOTEL CANINO- K9 SUITE! </label><br><br> 
+<label id="titulo">ModIn  </label><br><br> 
 <img src="http://img.ifreepic.com/930/32930_icon.jpg" alt="Dog anime" style="width:304px;height:228px;" id="imagen">
 <img src="https://image.freepik.com/iconos-gratis/huella-de-perro-en-un-corazon_318-38076.jpg" alt="Dog anime" style="width:50px;height:40px;" id="indicacion"><label id="indicacion">De click en la huella de lo que quiera ingresar </label>
 <br><br><input type=image src="https://image.freepik.com/iconos-gratis/huella-de-perro-en-un-corazon_318-38076.jpg" width="50" height="40" id="letras" onclick="nueva(http://localhost/ProyectoFinal/informacion.php)"><label id="letras"><a href="informacion.php" target="_blank">Informacion de Servicios </a></label><br><br>
@@ -36,9 +30,9 @@ and open the template in the editor.
 <input type=image src="https://image.freepik.com/iconos-gratis/huella-de-perro-en-un-corazon_318-38076.jpg" width="50" height="40" id="letras"><label id="letras"><a href="galeria.php" target="_blank">Galeria </a> </label> <br><br>
 </div>
 
-    .
+    
 
-</form>  
-     
+</form> 
+
     </body>
 </html>
--- PF/servicios.php
+++ PF/servicios.php
@@ -5,15 +5,42 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="estilos.css">
-        <title></title>
-    </head>
-    <body style="background-color:cornsilk;">
-       
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.140445802514!2d-74.09886548514571!3d4.568770696680712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f98eb3b47ca5f%3A0x54ff787e8b4fe069!2sParque+Serafina+III!5e0!3m2!1ses!2ses!4v1496192706510" width="600" height="450" frameborder="0" style="border:0" allowfullscreen id="imagen"></iframe>
-        
-    </body>
+   <head>
+     <script src="jquery-3.2.1.min.js" type="text/javascript"></script> 
+      <script src="jquery-ui.js"></script>  
+      <script>
+      $( function() {
+    	    $( "#objeto" ).draggable(
+    	    	{
+        	    	drag: function(){
+    	            var offset = $(this).offset();
+    	            var xPos = offset.left;
+    	            var yPos = offset.top;
+    	            $('#posicionX').text('x: ' + xPos);
+    	            $('#posicionY').text('y: ' + yPos);
+    	        	}
+	        
+    	    	}
+    	    );
+    	  } );
+      </script>
+      <style type="text/css">
+      		
+      		#objeto{
+      			width:100px;
+      			height:100px;
+      			background-color:#334444;
+      			margin:0 auto;
+      		}
+      </style>
+   </head>
+<body>
+    <div id="p" style="width:10px; height:10px; background-color:#0000ff; position:absolute"></div>
+       <a href="http://www.desarrolloweb.com/">DesarrolloWeb.com</a>
+           <div id="objeto">    
+    </div>    
+    <div id="posicionX"></div>
+    <div id="posicionY"></div>
+     
+</body>
 </html>
